b2b468417ddff62b446250892729243ff2476c9b
Cody Cogbill <codeman24@gmail.com>
2023-03-22 19:50:40 -0600

    arithmatic lab done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+ let num1 = 31
+let num2 = 2
+let  multiply = num1 * num2
+//
+let num3 = 45
+let num4 = 67
 
-// Write your code here
+let random = num3 + num4
+
+let num5 = 22
+let num6 = 6
+let mod = num5 % num6
+
+
+let max = num5- num2
